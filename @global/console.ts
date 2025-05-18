e2985eabda2f1d59906849fb45633cd196744742
Lel Illumina <dragonbilal938@outlook.com>
2025-05-18 22:14:32 +0500

    feat: fancy console.log

---
+++ @global/console.ts
@@ -0,0 +1,14 @@
+console.log(
+	"%cHey!\n",
+
+	/* CSS */ `
+    @import url('https://fonts.googleapis.com/css2?family=Gloock&display=swap');
+    color: darkgoldenrod;
+    font-family: 'Gloock', system-ui, serif;
+    font-size: 4rem;
+    -webkit-text-stroke: 1px white;
+    font-weight: bold;
+    `,
+
+	"You can view the proper sourcecode for this site on my GitHub: https://github.com/LelIllumina/NueNekos/ and for my old site on https://github.com/LelIllumina/LelIllumina.github.io/",
+);
